a6129037aa0e2c4337cf0ffb640b23d0e019f476
Pratik Soni <pratiksoni916@gmail.com>
2018-06-23 20:37:09 +0530

    Added pages in books.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Ruby' }
+        { title: 'Javascript: The Good Parts', pages: 100 },
+        { title: 'Harry Potter', pages: 200 },
+        { title: 'The Dark Tower', pages: 38 },
+        { title: 'Eloquent Ruby', pages: 205 }
     ];
 }
